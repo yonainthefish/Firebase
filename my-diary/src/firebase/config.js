5ce2391b6772ab19f5ecfeacf223bcf2d6661ca1
yonainthefish <yonaisgood@gmail.com>
2023-08-15 17:49:27 +0900

    Feat: firebase 설정

---
+++ my-diary/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnReM3Q5LCvP_MrLD-jO4nFY5Fkvl7mBA",
+  authDomain: "mydiary-c616b.firebaseapp.com",
+  projectId: "mydiary-c616b",
+  storageBucket: "mydiary-c616b.appspot.com",
+  messagingSenderId: "241536025639",
+  appId: "1:241536025639:web:2e8e5bcfd097364dacfa02",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
